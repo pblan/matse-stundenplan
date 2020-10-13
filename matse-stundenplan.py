aeb2be4fcc40e642d4bc891fc0f76ee8197c11e5
Patrick Gustav Blaneck <patrick.blaneck@rwth-aachen.de>
2020-10-13 16:26:23 +0200

    added fetch

--- matse-stundenplan.py
+++ matse-stundenplan.py
@@ -5,12 +5,37 @@ import uuid
 import os
 import pytz
 
-courses_dict = {'Betriebswirtschaftslehre': 'bwl',
+COURSES_DICT = {'Mathematische Grundlagen': 'mathe_grundlagen',
+                'Vorstellung der Sozialberatung': 'sozialberatung',
+                'Klausureinsicht': 'klausureinsicht',
+                'Termine Umweltschutz- und Arbeitssicherheitsseminare': 'umweltschutz',
+                'Java-Blockkurs': 'java_block',
+                'Wahl des Semestersprechers': 'semestersprecher',
+                'Selbstlernphase für asynchrone Videovorlesung (Analysis 1)': 'ana1_async',
+                'Selbstlernphase für asynchrone Videovorlesung (Lineare Algebra 1)': 'la1_async',
+                'Selbstlernphase für asynchrone Videovorlesung (Java)': 'java_async',
+                'Selbstlernphase für asynchrone Videovorlesung (Stochastik)': 'sto_async',
+                'Selbstlernphase für asynchrone Videovorlesung (Softwaretechnik)': 'swt_async',
+                'Java-Blockkursklausur': 'java_block_klausur',
+                'Erstievent': 'erstievent',
+                'IT-Grundlagen': 'it_grundlagen',
+                'Kenntnistest': 'kenntnistest',
+                'Tutorium Java': 'tutorium_java',
+                'Übung 1. Lehrjahr': 'uebung_1',
+                'Übung 2. Lehrjahr': 'uebung_2',
+                'Datenbanken': 'db',
+                'Softwaretechnik': 'swt_sync',
+                'SWT-Messe': 'swt_messe',
+                'Stochastik': 'sto_sync',
+                'IHK-Zwischenprüfung': 'ihk_zwischenpruefung',
+                'IHK-Abschlussprüfung': 'ihk_abschlusspruefung',
+                'Prüfungsvorbereitung': 'pruefungsvorbereitung',
+                'Betriebswirtschaftslehre': 'bwl',
                 'C#': 'csharp',
                 'Technisches Englisch': 'technisches_englisch',
                 'Software Development in a Customer-Supplier Relation': 'sd_in_a_csr',
                 'Fortran': 'fortran',
-                'Einführung in die stochastischen Prozesse': 'einfuehrung_stoch_prozesse',
+                'Einführung in die stochastischen Prozesse': 'einfuehrung_stochastische_prozesse',
                 'Prozessorientiertes Qualitätsmanagement (TÜV)': 'tuev',
                 'C++': 'cpp',
                 'Machine Learning': 'machine_learning',
@@ -40,7 +65,6 @@ def adjust_json_date(date, offset = -1):
         hour_str = hour
 
     return "{}{}{}T{}{}{}Z".format(year, month, day, hour_str, minute, sec)
-
 
 def adjust_datetime_date(date, offset = -1):
     year, month, day, hour, minute, sec = date.year, date.month, date.day, date.hour, date.minute, date.second
@@ -73,8 +97,29 @@ def adjust_datetime_date(date, offset = -1):
 
     return "{}{}{}T{}{}{}Z".format(year, month_str, day_str, hour_str, minute_str, sec_str)
 
+def add_event(calendar, event, name, now = datetime.now()):
+    if ("Feiertag" in event['title']):
+            return
+    if (name != None and name not in event['title']):
+        return
 
-def build(id, name=None):
+    # Create ICS/iCalendar events
+    calendar.append('BEGIN:VEVENT\r\n')
+    calendar.append('DTSTART:{}\r\n'.format(adjust_json_date(event['start'])))
+    calendar.append('DTEND:{}\r\n'.format(adjust_json_date(event['end'])))
+    calendar.append('DTSTAMP:{}\r\n'.format(adjust_datetime_date(now)))
+    calendar.append('UID:{}-{}\r\n'.format(COURSES_DICT[name], adjust_json_date(event['start'])))
+    calendar.append('CREATED:{}\r\n'.format(adjust_datetime_date(now)))
+    calendar.append('DESCRIPTION:{}\r\n'.format(event['information']))
+    calendar.append('LAST-MODIFIED:{}\r\n'.format(adjust_datetime_date(now)))
+    calendar.append('SEQUENCE:0\r\n')
+    calendar.append('SUMMARY:{}\r\n'.format(event['title']))
+    calendar.append('LOCATION:{}\r\n{} {}\r\n{}\r\n'.format(
+        event['location']['name'], event['location']['street'], event['location']['nr'], event['location']['desc']))
+    calendar.append('TRANSP:TRANSPARENT\r\n')
+    calendar.append('END:VEVENT\r\n')
+
+def build(id, name):
     print("Chosen calendar: ", id, " ", name)
     print("Starting to build new .ics file...")
 
@@ -98,36 +143,13 @@ def build(id, name=None):
     str_list.append('X-WR-TIMEZONE:{}\r\n'.format(tz))
 
     for x in dict:
-        if ("Feiertag" in x['title']):
-            continue
-        if (name != None and name not in x['title']):
-            continue
-
-        # Create ICS/iCalendar events
-        str_list.append('BEGIN:VEVENT\r\n')
-        str_list.append('DTSTART:{}\r\n'.format(adjust_json_date(x['start'])))
-        str_list.append('DTEND:{}\r\n'.format(adjust_json_date(x['end'])))
-        str_list.append('DTSTAMP:{}\r\n'.format(adjust_datetime_date(now)))
-        str_list.append('UID:{}\r\n'.format(str(uuid.uuid4())))
-        str_list.append('CREATED:{}\r\n'.format(adjust_datetime_date(now)))
-        str_list.append('DESCRIPTION:{}\r\n'.format(x['information']))
-        str_list.append('LAST-MODIFIED:{}\r\n'.format(adjust_datetime_date(now)))
-        str_list.append('SEQUENCE:0\r\n')
-        # str_list.append('RRULE:FREQ=YEARLY;INTERVAL=1')
-        str_list.append('SUMMARY:{}\r\n'.format(x['title']))
-        str_list.append('LOCATION:{}\r\n{} {}\r\n{}\r\n'.format(
-            x['location']['name'], x['location']['street'], x['location']['nr'], x['location']['desc']))
-        str_list.append('TRANSP:TRANSPARENT\r\n')
-        str_list.append('END:VEVENT\r\n')
+        add_event(str_list, x, name)
 
     str_list.append('END:VCALENDAR')
 
     # Output ICS File
-    if (name == None):
-        calendar_path = os.path.expanduser("~") + '/html/dev/matse/{}/calendar.ics'.format(id)
-    else:
-        calendar_path = os.path.expanduser("~") + '/html/dev/matse/{}/{}_calendar.ics'.format(id,courses_dict(name))
-    
+    calendar_path = os.path.expanduser("~") + '/html/dev/matse/{}/{}_calendar.ics'.format(id, COURSES_DICT[name])
+
     with open(calendar_path, 'wb') as f:
         content = ''.join(str_list)
         content = re.sub(r"None", "", content)
@@ -140,15 +162,15 @@ def build(id, name=None):
         print(os.path.realpath(f.name))
 
     print("Done writing data!")
-
-def fetch_courses():
+    
+def fetch_courses(id):
     now = datetime.now()
 
     start = "{}-09-01".format(now.year)
     end = "{}-08-30".format((now + timedelta(days=366)).year)
 
-    url = "https://www.matse.itc.rwth-aachen.de/stundenplan/web/eventFeed/4?start={}&end={}".format(
-        start, end)
+    url = "https://www.matse.itc.rwth-aachen.de/stundenplan/web/eventFeed/{}?start={}&end={}".format(
+        id, start, end)
     r = requests.get(url)
     dict = r.json()
     print("Got data...")
@@ -158,11 +180,27 @@ def fetch_courses():
     for x in dict:
         if (re.search("Feiertag", x['title']) != None):
             continue
-        s.add(x['title'])
+        
+        if (re.search("Selbstlernphase für asynchrone Videovorlesung", x['title']) != None):
+            s.add(x['title'] + " (" + re.sub(r"<br />*\s*", "", x['information']) + ")")
+        elif (re.search("Selbstlernphase für asynchrone Videovorlesung", x['title']) != None):
+            s.add(x['title'] + " (" + re.sub(r"<br />*\s*", "", x['information']) + ")")
+        else: 
+            s.add(x['title'])
     return s
 
-#build(1)
+for course in fetch_courses(1):
+    if (course not in COURSES_DICT): print("### MISSING ENTRY: " + course)
+    build(1, course)
+    
+for course in fetch_courses(2):
+    if (course not in COURSES_DICT): print("### MISSING ENTRY: " + course)
+    
+for course in fetch_courses(3):
+    if (course not in COURSES_DICT): print("### MISSING ENTRY: " + course)
+    
+for course in fetch_courses(4):
+    if (course not in COURSES_DICT): print("### MISSING ENTRY: " + course)
+
 #build(2)
 #build(3)
-for course in fetch_courses():
-    build(4,course)
